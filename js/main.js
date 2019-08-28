99d04f3c641395efd8db1b48e63909bddb7d2835
Muhammad Reda Abdelaal <trans.muhammad@gmail.com>
2019-08-28 10:00:40 +0200

    adding more features to navbar

--- js/main.js
+++ js/main.js
@@ -1,26 +1,48 @@
 
 
 
-
-
-// change navbar background color when reching about section
-$(window).scroll(function () { 
-
-    let scroll = $(window).scrollTop(); // get scrollTop value
+$(window).scroll(function () { //on scroll change navbar background color and nav links colors
+    let scroll = $(window).scrollTop();
+    let sections = $('section');
+    let sectionsArray = Array.from(sections);
+    let id;
     
-    if (scroll > ($('#about').offset().top)-20) {// when reach scrollTop of about
-        $('nav').css('backgroundColor', '#191919');// change navbar background to dark
-    } else {
-        $('nav').css('backgroundColor', 'transparent');// else change navbar background to transparent
-    }
+    for(let i =0; i < sectionsArray.length; i++) {
+        id = sectionsArray[i].id;
+        /**
+         * when reach top offset of about section
+         * change navbar background to dark
+         * else change navbar background to transparent
+         */
+        if (scroll > ($(`#${sectionsArray[1].id}`).offset().top)-20) { 
+            $('nav').css('backgroundColor', 'rgba(25, 25, 25, 0.95)'); 
+        } else {
+            $('nav').css('backgroundColor', 'transparent');
+        }
 
+        /**
+         * when reach top offset of any section
+         * if scroll exceeded it
+         * add active class to the nav link corrosponding to that section
+         * and remove the sama class from its siblings
+         */
+        if (scroll > $(`#${id}`).offset().top) {
+            $(`nav a[href='#${id}']`).parent().addClass('active');
+            $(`nav a[href='#${id}']`).parent().siblings().removeClass('active');
+        }
+    
+    }
 });
 
-// navbar links animated scroll
+/**
+ * on clicking any nav link scroll to its corrosponding section with animation
+ * and add active class to the nav item of this link
+ * and remove active class from its parents' sibilins if any
+ */
 $('nav a').click(function(){
-    $(this).parent().addClass('active'); //get the clicked nav link and add active class to its parent
-    $(this).parent().siblings().removeClass('active'); // remove active class from its parents' sibilins if any
-    let aHref = $(this).attr("href"); // get the href of clicked nav link
-    let aboutOffset = $(aHref).offset().top; // get the top offset of section corrosponding to the clicked nav link
-    $('body, html').animate({scrollTop: aboutOffset}, 1000); // scroll the body moving to that section
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    let aHref = $(this).attr("href");
+    let offset = ($(aHref).offset().top - 10);
+    $('body, html').animate({scrollTop: offset}, 1000);
 });
